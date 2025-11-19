b139e46962ac59cb12b7064b758d80088e900c6e
balajipagadala64 <balajipagadala64@gmail.com>
2025-11-20 00:03:28 +0530

    Improved dashboard screen navigation

--- app/src/main/java/balaji/project/cryptopricetracker/HomeActivity.kt
+++ app/src/main/java/balaji/project/cryptopricetracker/HomeActivity.kt
@@ -1,13 +1,26 @@
 package balaji.project.cryptopricetracker
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+           HomeScreen()
+        }
     }
+}
+
+
+@Composable
+fun HomeScreen()
+{
+
 }
